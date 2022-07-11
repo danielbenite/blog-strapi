059ea3058ecce17db47a8211a66f8155dca0955e
danielbenite <dan.gonzagart@outlook.com>
2022-07-11 18:02:59 -0300

    creating slug.js

---
+++ pages/posts/[slug].js
@@ -0,0 +1,62 @@
+import Tag from "../../components/Tag";
+
+export default function Post() {
+    return (
+        <>
+            <section className="post--wrapper container">
+                <section>
+                    <div className="post--ad_one"></div>
+                </section>
+
+                <div><span></span></div>
+
+                <section>
+                    <article>
+
+                        <div>
+                            <img />
+                        </div>
+
+                        <div className="post--article_title">
+                            <span>entretenimento</span>
+                            <h2>Stranger Things 4: Netflix revela prévia do volume 2 da quarta temporada</h2>
+                            <p></p>
+                            <div>
+                                <div>
+                                    <div>
+                                        <img />
+                                    </div>
+                                    <p></p>
+                                </div>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <p></p>
+                        </div>
+
+                        <div>
+                            <div>
+                                <img />
+                            </div>
+
+                            <div>
+                                <div>
+                                    <img />
+                                    <h2></h2>
+                                </div>
+                                <div>
+                                    <p></p>
+                                    <img />
+                                </div>
+                            </div>
+
+                        </div>
+                    </article>
+                </section>
+            </section>
+        </>
+    )
+}
